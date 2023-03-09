e4f6ac492d624f6df988268bf86494eaddbee286
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-03-09 22:31:24 +0200

    Create files

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
