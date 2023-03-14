08aaa7792870c47352558dd90be68476999cc1c7
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-03-14 14:22:10 +0200

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+    return tuple(map(sum, zip(tuple_a[:2], tuple_b[:2])))
