6fdc06c4754163b548f51810769eaebabab48cfd
Tinashe Matanda <nash@cybernash.cybernash.org>
2023-05-17 20:30:38 +0200

    SQL ALL

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Displays the average temperature (in Fahrenheit) by city ordered by desc temp --
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
