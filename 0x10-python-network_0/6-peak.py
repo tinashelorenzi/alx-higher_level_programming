63211636138f24169545c08eada59ce6595d7a83
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-07-03 06:20:41 +0000

    Update search algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,26 +3,23 @@
    this function finds a peak in a list of unsorted integers
 """
 
-
-def find_peak(numbr):
-    '''
-        Finds the peak in a list of numbers
-    '''
-    length = len(numbr)
+def find_peak(numbers):
+    """
+    Finds the peak in a list of numbers using an optimized binary search approach.
+    """
+    length = len(numbers)
     if length == 0:
         return None
-    if length == 1:
-        return (numbr[0])
-    if length == 2:
-        return numbr[0] if numbr[0] >= numbr[1] else numbr[1]
 
-    for idx in range(0, length):
-        value = numbr[idx]
-        if (idx > 0 and idx < length - 1 and
-                numbr[idx + 1] <= value and numbr[idx - 1] <= value):
-                return value
-        elif idx == 0 and numbr[idx + 1] <= value:
-            return value
-        elif idx == length - 1 and numbr[idx - 1] <= value:
-            return value
-    return None
+    low = 0
+    high = length - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if numbers[mid] < numbers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return numbers[low]
