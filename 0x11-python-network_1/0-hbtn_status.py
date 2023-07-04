b1ace36dbbc2bc508262005004dd45fece2af2f8
Tinashe <tinashelorenzi@protonmail.com>
2023-07-04 19:49:30 +0200

    Update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,14 @@
 fetches https://intranet.hbtn.io/status.
 """
 
+import urllib.request
 
-if __name__ == '__main__':
-    import urllib.request
+url = "https://alx-intranet.hbtn.io/status"
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print("    - type:", type(body))
+print("    - content:", body)
+print("    - utf8 content:", body.decode('utf-8'))
